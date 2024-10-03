3a239b4bcbec831b2fc06f71fc6a06db09392b92
Milad Nowshahr <miladnowshahr@yahoo.com>
2024-10-03 19:10:48 +0330

    Users Module

---
+++ Evently.Module.Event.Application/Events/RescheduleEvent/EventRescheduledDomainEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Evently.Common.Application.Messaging;
+using Evently.Modules.Events.Domain.Events;
+
+namespace Evently.Modules.Event.Application.Events.RescheduleEvent;
+internal sealed class EventRescheduledDomainEventHandler : IDomainEventHandler<EventRescheduledDomainEvent>
+{
+    public Task Handle(EventRescheduledDomainEvent notification, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
--- Evently.Module.Events.Infrastructure/EventsModule.cs
+++ Evently.Module.Events.Infrastructure/EventsModule.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using Evently.Common.Infrastructure.Interceptors;
 using Evently.Common.Presentation.Endpoints;
 using Evently.Modules.Events.Application;
 using Evently.Modules.Events.Application.Abstractions.Data;
@@ -31,7 +32,7 @@ public static class EventsModule
 {
     public static void MapEndpoints(IEndpointRouteBuilder app)
     {
-        
+
     }
 
     public static IServiceCollection AddEventsModule(this IServiceCollection services, IConfiguration configuration)
@@ -46,12 +47,16 @@ public static class EventsModule
     {
         string databaseConfigurationString = configuration.GetConnectionString("Database");
 
-        services.AddDbContext<EventsDbContext>(options =>
-             options.UseNpgsql(databaseConfigurationString,
+        services.AddDbContext<EventsDbContext>((sp, options) =>
+        {
+            options.UseNpgsql(databaseConfigurationString,
                 npgsqlOption => npgsqlOption.MigrationsHistoryTable(HistoryRepository.DefaultTableName, Schemes.Events))
-             .UseSnakeCaseNamingConvention());
+             .UseSnakeCaseNamingConvention()
+            .AddInterceptors(sp.GetRequiredService<PublishDomainEventsInterceptor>());
+        });
+    
 
-        services.AddScoped<IUnitofWork>(sp => sp.GetRequiredService<EventsDbContext>());
+    services.AddScoped<IUnitofWork>(sp => sp.GetRequiredService<EventsDbContext>());
         services.AddScoped<IEventRepository, EventRepository>();
         services.AddScoped<ITicketTypeRepository, TicketTypeRepository>();
         services.AddScoped<ICategoryRepository, CategoryRepository>();
